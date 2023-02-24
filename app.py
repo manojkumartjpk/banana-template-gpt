e5e2bfbdc2c3c289320b1f2570e5858ff92f6c9b
Manoj <manoj@peakperformer.io>
2023-02-24 10:52:02 +0530

    changing to got 125

--- app.py
+++ app.py
@@ -10,7 +10,12 @@ def init():
     global tokenizer
 
     print("loading to CPU...")
-    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model = GPTJForCausalLM.from_pretrained(
+        "EleutherAI/gpt-neo-125M",
+        revision="float16",
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+    )
     print("done")
 
     # conditionally load to GPU
@@ -19,20 +24,20 @@ def init():
         model.cuda()
         print("done")
 
-    tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    tokenizer = GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
 
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
-def inference(model_inputs:dict) -> dict:
+def inference(model_inputs: dict) -> dict:
     global model
     global tokenizer
 
     # Parse out your arguments
-    prompt = model_inputs.get('prompt', None)
+    prompt = model_inputs.get("prompt", None)
     if prompt == None:
-        return {'message': "No prompt provided"}
-    
+        return {"message": "No prompt provided"}
+
     # Tokenize inputs
     input_tokens = tokenizer.encode(prompt, return_tensors="pt").to(device)
 
@@ -40,7 +45,7 @@ def inference(model_inputs:dict) -> dict:
     output = model.generate(input_tokens)
 
     # Decode output tokens
-    output_text = tokenizer.batch_decode(output, skip_special_tokens = True)[0]
+    output_text = tokenizer.batch_decode(output, skip_special_tokens=True)[0]
 
     result = {"output": output_text}
 
--- download.py
+++ download.py
@@ -6,17 +6,22 @@
 from transformers import GPTJForCausalLM, GPT2Tokenizer
 import torch
 
+
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
     print("downloading model...")
     GPTJForCausalLM.from_pretrained(
-        "EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True
+        "EleutherAI/gpt-neo-125M",
+        revision="float16",
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
     )
     print("done")
 
     print("downloading tokenizer...")
-    GPT2Tokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+    GPT2Tokenizer.from_pretrained("EleutherAI/gpt-neo-125M")
     print("done")
+
 
 if __name__ == "__main__":
     download_model()
